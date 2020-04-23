eeaa368cd5d33076d92082b7a295f9795abcb99d
bipulmanjhi001 <bipulmanjhi@gmail.com>
2020-04-23 11:03:37 +0530

    Initial commits

---
+++ app/src/main/java/joharmis/com/Market_Charges.java
@@ -0,0 +1,28 @@
+package joharmis.com;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class Market_Charges extends AppCompatActivity {
+
+    Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_market__charges);
+
+        back=(Button)findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Market_Charges.this,Market_Profile.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
